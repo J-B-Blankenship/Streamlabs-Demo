7ff003a88bb3727bc7995b03ac1eed3a2bc62570
J-B-Blankenship <jblankenship9824@gmail.com>
2022-04-18 21:25:25 -0500

    Successfully linked obs-studio to project.

--- binding.gyp
+++ binding.gyp
@@ -4,6 +4,9 @@
             "target_name": "hello",
             "cflags!": [ "-fno-exceptions"],
             "cflags_cc!": [ "-fno-exceptions" ],
+            "libraries": [
+            	"../third_party/obs-studio/libobs/libobs.so"
+            ],
             "sources": [ "cpp/Hello.cpp"],
             "include_dirs": [
                 "<!@(node -p \"require('node-addon-api').include\")"
--- cpp/Hello.cpp
+++ cpp/Hello.cpp
@@ -1,9 +1,14 @@
 #include <napi.h>
+#include "../third_party/obs-studio/libobs/obs.h"
 
 Napi::String Method(const Napi::CallbackInfo& info)
 {
     Napi::Env environment{ info.Env() };
-    return Napi::String::New(environment, "world");
+    if(obs_startup())
+    {
+        return Napi::String::New(environment, "world");
+    }
+    return Napi::String::New(environment, "not linked");
 }
 
 Napi::Object Init(Napi::Env environment, Napi::Object exports)
